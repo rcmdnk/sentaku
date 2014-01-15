911e12d0b6b5a30a4af508f2253d3e7e0e03e38e
rcmdnk <rcmdnk@gmail.com>
2014-01-15 19:22:17 +0100

    first import

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+scripts=(https://raw.github.com/rcmdnk/sentaku/master/bin/sentaku)
+if [ x"$prefix" = x ];then
+  prefix=/usr/local
+fi
+
+prefix=`echo $prefix|sed 's|--prefix=||'|sed "s|^~|$HOME|"|sed "s|^\.|$(pwd)|"`
+
+echo
+echo "###############################################"
+echo "Install to $prefix/bin"
+echo "###############################################"
+echo
+sudo=""
+if [ -d $prefix/bin ];then
+  touch $prefix/bin/.install.test >& /dev/null
+  if [ $? -ne 0 ];then
+    sudo=sudo
+  else
+    rm -f $prefix/bin/.install.test
+  fi
+else
+  mkdir -p $prefix/bin>&  /dev/null
+  if [ $? -ne 0 ];then
+    sudo mkdir -p $prefix/bin
+    sudo=sudo
+  fi
+fi
+
+for s in ${scripts[@]};do
+  sname=`basename $s`
+  echo Intalling ${sname}...
+  $sudo curl -fsSL -o $prefix/bin/$sname $s
+done
