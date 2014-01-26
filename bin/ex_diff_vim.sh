d0b3d5c5b41717a5271435ca1146841f9e184bab
rcmdnk <rcmdnk@gmail.com>
2014-01-26 03:23:56 +0100

    bin/ex_diff_vim.sh    bin/sentaku, from darwin13.0.0

--- bin/ex_diff_vim.sh
+++ bin/ex_diff_vim.sh
@@ -9,20 +9,19 @@ _sf_execute () { # {{{
 } # }}}
 
 _sf_select () { # {{{
-  local dirs=(${_s_inputs[$_s_current_n]})
-  if [ "${#dirs[@]}" -ne 2 ];then
-    _sf_echo "Sorry, directory path or file name have space, can not recognize."
-  else
-    vim -d "${dirs[0]}" "${dirs[1]}" </dev/tty
-  fi
+  vim -d "$_s_first_dir/${_s_inputs[$_s_current_n]}"\
+    "$_s_second_dir/${_s_inputs[$_s_current_n]}" </dev/tty
+  tput civis >/dev/tty 2>/dev/null || tput vi >/dev/tty 2>/dev/null
 } # }}}
 
 _sf_d () {
+  local n=$_s_current_n
   if [ $_s_n -eq 1 ];then
     _sf_echo "There is no remained diff"
     _sf_quit
   elif [ $_s_current_n -eq $((_s_n-1)) ];then
     _sf_n_down
+    _s_is_print=1
   fi
   local orig_ifs=$IFS
   IFS="$_s_s"
@@ -30,7 +29,7 @@ _sf_d () {
   inputs=()
   local i
   for ((i=0; i<$_s_n; i++));do
-    if [ $i -ne $_s_current_n ];then
+    if [ $i -ne $n ];then
       inputs=("${inputs[@]}" "${_s_inputs[$i]}")
     fi
   done
@@ -39,9 +38,41 @@ _sf_d () {
   _s_n=${#_s_inputs[@]}
 }
 
+_sf_setheader () { # {{{
+  _s_show="$_s_first_dir"
+  _sf_show 0 $((_s_cols-3))
+  local a_dir="$_s_show"
+  _s_show="$_s_second_dir"
+  _sf_show 0 $((_s_cols-3))
+  local b_dir="$_s_show"
+  _s_header=""
+  if [ $_s_noheader = 1 -o $_s_lines -lt 10 ];then
+    return
+  elif [ $_s_cols -ge 66 ];then
+    _s_header=" $_s_n files are different in
+a:$a_dir
+b:$b_dir
+ [n](n-down), [n]k(n-up), gg(top), G(bottom), [n]gg/G, (go to n),
+ ^D(lf page down), ^U(Half page up), ^F(Page down), ^B(Page Up),
+ d(delete from the list), Enter/Space(vim diff), q/Esc(quit)
+"
+  elif [ $_s_cols -ge 40 ];then
+      _s_header=" $_s_n values in total
+a:$a_dir
+b:$b_dir
+ vimike updown, e.g)j:up, k:down, gg/G
+ d(delete from the list),
+ Enter/Space(vim diff), q/Esc(quit)
+"
+  fi
+}  # }}}
+
 if [ $# -ne 2 ];then
   echo "usage: ex_diff_vim dir1 dir2"
   exit 1
 fi
+_s_first_dir="${1%/}/"
+_s_second_dir="${2%/}/"
 
-diff -r "$1" "$2" |grep diff |cut -d' ' -f3-|_sf_main
+diff -r "$_s_first_dir" "$_s_second_dir" |grep diff |grep -v Binary|\
+  sed "s!.*${_s_second_dir}!!g"|_sf_main
